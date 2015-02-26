392f87a49135186757b701c6dd811952c7b99662
Julien Christin <jul.christin@gmail.com>
2015-02-25 22:20:34 -0500

    Add left panel to contain settings

--- react_components/app.js
+++ react_components/app.js
@@ -3,12 +3,14 @@
 "use strict";
 
 var React = require("react"),
-	Sidebar = require("./sidebar");
+	Sidebar = require("./sidebar"),
+	Panel = require("./panel");
 
 module.exports = React.createClass({
 	render: function() {
 		return React.createElement("div", null,
-			React.createElement(Sidebar)
+			React.createElement(Sidebar),
+			React.createElement(Panel)
 		);
 	}
 });
