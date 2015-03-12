cfab66ea03993c5acaa28575407b330320fe4d5a
Julien Christin <jul.christin@gmail.com>
2015-03-11 22:06:40 -0400

    Add a slider to the price component

--- react_components/price.js
+++ react_components/price.js
@@ -1,13 +1,40 @@
 "use strict";
 
-var React = require("react");
+var React = require("react"),
+	actions = require("../react_stores/actions"),
+	$ = require("jquery-ui"),
+	$ = require("jquery");
 
 module.exports = React.createClass({
+	getInitialState: function() {
+		return {
+			price: [0, 4000]
+		};
+	},
+	componentDidMount: function() {
+		$("#price").slider({
+			range: true,
+			min: 0,
+			max: 4000,
+			step: 50,
+			values: [500, 1000],
+			slide: function(event, ui) {
+				actions.setPrice(ui.values);
+				this.setState({
+					price: ui.values
+				});
+			}.bind(this)
+		});
+	},
 	render: function() {
 		return React.createElement("div", {
-			style: {
-				background: "red"
-			}
-		}, "price");
+				className: "price"
+			},
+			React.createElement("div", {
+				className: "caption"
+			}, this.state.price[0] + "$ - " + this.state.price[1] + "$"),
+			React.createElement("div", {
+				id: "price"
+			}));
 	}
 });
