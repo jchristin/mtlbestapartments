b74103b13718bfd57ba13053301abb0069ba491a
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2015-04-11 19:00:59 -0400

    Added dorval borough.

--- react_stores/zone-store.js
+++ react_stores/zone-store.js
@@ -12,6 +12,7 @@ module.exports = Reflux.createStore({
 			"anjou": require("../boroughs/anjou"),
 			"cote-des-neiges-notre-dame-de-grace": require("../boroughs/cote-des-neiges-notre-dame-de-grace"),
 			"cote-saint-luc": require("../boroughs/cote-saint-luc"),
+			"dorval": require("../boroughs/dorval"),
 			"hampstead": require("../boroughs/hampstead"),
 			"lachine": require("../boroughs/lachine"),
 			"lasalle": require("../boroughs/lasalle"),
