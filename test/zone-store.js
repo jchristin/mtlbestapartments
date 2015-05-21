455f118ee5b4ad586b6de5cf181ae9be035245b3
Julien Christin <jul.christin@gmail.com>
2015-05-21 07:38:42 -0400

    Test is all boroughs are closed

--- test/zone-store.js
+++ test/zone-store.js
@@ -10,4 +10,10 @@ describe("zoneStore", function() {
 	it("should have 33 boroughs", function() {
 		_.size(zoneStore.boroughs).should.be.equal(33);
 	});
+
+	it("should have closed boroughs", function() {
+		_.forEach(zoneStore.boroughs, function (value) {
+			_.first(value).should.be.eql(_.last(value));
+		});
+	});
 });
