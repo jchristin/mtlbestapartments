8d2a69c4d62473b1c17f31c2d5d7b0bf96e69eb8
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2015-09-28 21:35:30 -0400

    First draft for the staff pick page (need to work on the CSS)

--- react_components/staff-picks.js
+++ react_components/staff-picks.js
@@ -2,10 +2,33 @@
 
 "use strict";
 
-var React = require("react");
+var React = require("react"),
+	Reflux = require("reflux"),
+	apartStore = require("../react_stores/apart-store"),
+	infoBoxComponent = require("./info-box"),
+	_ = require("lodash");
 
 module.exports = React.createClass({
+	mixins: [
+		Reflux.listenTo(apartStore, "onMapDataChange"),
+	],
+	onMapDataChange: function(filteredApt) {
+		var aparts = _.map(filteredApt, function(apart) {
+			return React.createElement(infoBoxComponent, {
+				apart: apart
+			});
+		});
+
+		this.setState({
+			content: aparts
+		});
+	},
+	getInitialState: function() {
+		return {
+			content: ""
+		};
+	},
 	render: function() {
-		return React.createElement("div", null, "STAFF PICKS");
+		return React.createElement("div", null, this.state.content);
 	}
 });
