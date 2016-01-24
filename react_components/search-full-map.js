b4856d7251335023c39370544582cbf0448937bf
Julien Christin <jul.christin@gmail.com>
2016-01-24 14:14:38 -0500

    Remove duplicated boroughs index

--- react_components/search-full-map.js
+++ react_components/search-full-map.js
@@ -3,46 +3,11 @@
 "use strict";
 
 var React = require("react"),
-	_ = require("lodash");
+	_ = require("lodash"),
+	boroughs = require("../boroughs");
 
 module.exports = React.createClass({
 	componentDidMount: function() {
-		this.boroughs = {
-			"ahuntsic-cartierville": require("../boroughs/ahuntsic-cartierville"),
-			"anjou": require("../boroughs/anjou"),
-			"baie-durfe": require("../boroughs/baie-durfe"),
-			"beaconsfield": require("../boroughs/beaconsfield"),
-			"cote-des-neiges-notre-dame-de-grace": require("../boroughs/cote-des-neiges-notre-dame-de-grace"),
-			"cote-saint-luc": require("../boroughs/cote-saint-luc"),
-			"dollard-des-ormeaux": require("../boroughs/dollard-des-ormeaux"),
-			"dorval": require("../boroughs/dorval"),
-			"hampstead": require("../boroughs/hampstead"),
-			"ile-bizard-sainte-genevieve": require("../boroughs/ile-bizard-sainte-genevieve"),
-			"kirkland": require("../boroughs/kirkland"),
-			"lachine": require("../boroughs/lachine"),
-			"lasalle": require("../boroughs/lasalle"),
-			"le-plateau-mont-royal": require("../boroughs/le-plateau-mont-royal"),
-			"le-sud-ouest": require("../boroughs/le-sud-ouest"),
-			"mercier-hochelaga-maisonneuve": require("../boroughs/mercier-hochelaga-maisonneuve"),
-			"montreal-est": require("../boroughs/montreal-est"),
-			"montreal-nord": require("../boroughs/montreal-nord"),
-			"montreal-ouest": require("../boroughs/montreal-ouest"),
-			"mont-royal": require("../boroughs/mont-royal"),
-			"outremont": require("../boroughs/outremont"),
-			"pierrefonds-roxboro": require("../boroughs/pierrefonds-roxboro"),
-			"pointe-claire": require("../boroughs/pointe-claire"),
-			"riviere-des-prairies-pointe-aux-trembles": require("../boroughs/riviere-des-prairies-pointe-aux-trembles"),
-			"rosemont-la-petite-patrie": require("../boroughs/rosemont-la-petite-patrie"),
-			"sainte-anne-de-bellevue": require("../boroughs/sainte-anne-de-bellevue"),
-			"saint-laurent": require("../boroughs/saint-laurent"),
-			"saint-leonard": require("../boroughs/saint-leonard"),
-			"senneville": require("../boroughs/senneville"),
-			"verdun": require("../boroughs/verdun"),
-			"ville-marie": require("../boroughs/ville-marie"),
-			"villeray-saint-michel-parc-extension": require("../boroughs/villeray-saint-michel-parc-extension"),
-			"westmount": require("../boroughs/westmount"),
-		};
-
 		this.allZone = [];
 
 		this.map = new google.maps.Map(
@@ -55,12 +20,11 @@ module.exports = React.createClass({
 		this.map.mapTypes.set("map-style", styledMap);
 		this.map.setMapTypeId("map-style");
 
-		_.forEach(
-			this.boroughs,
-			function(borough) {
-				var polygon = this.drawZone(borough);
+		_.forEach(boroughs, function(borough) {
+				var polygon = this.drawZone(borough.coord);
 				this.allZone.push(polygon);
-			}, this
+			},
+			this
 		);
 
 		this.map.fitBounds(this.bounds);
