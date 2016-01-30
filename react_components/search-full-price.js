0b203296d9171a81d0ae2b2d5c806550f3a6ad8d
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2016-01-30 15:53:31 -0500

    Beautify search price code + simplify search room code.

--- react_components/search-full-price.js
+++ react_components/search-full-price.js
@@ -7,53 +7,36 @@ var React = require("react"),
 
 module.exports = React.createClass({
 	getInitialState: function() {
-		return {
-			priceMin: 500,
-			priceMax: 1500,
-			stars: 5,
-		};
+		return {priceMin: 500, priceMax: 1500, stars: 5};
 	},
 	changeMinPrice: function(event) {
-		this.setState({
-			priceMin: event.target.value
-		});
+		this.setState({priceMin: event.target.value});
 	},
 	changeMaxPrice: function(event) {
-		this.setState({
-			priceMax: event.target.value
-		});
+		this.setState({priceMax: event.target.value});
 	},
 	render: function() {
 		return React.createElement("div", {
-				className: "edit-search",
-			},
-			React.createElement("div", null,
-				"Price between " + this.state.priceMin + "$ and " + this.state.priceMax + "$"),
-			React.createElement("input", {
-				type: "range",
-				name: "points",
-				min: "0",
-				max: "3950",
-				step: "50",
-				value: this.state.priceMin,
-				onChange: this.changeMinPrice
-			}),
-			React.createElement("input", {
-				type: "range",
-				name: "points",
-				min: "50",
-				max: "4000",
-				step: "50",
-				value: this.state.priceMax,
-				onChange: this.changeMaxPrice
-			}),
-			React.createElement("hr", null),
-			React.createElement(
-				starsLayout, {
-					stars: this.state.stars,
-					editable: true
-				}
-			)
-		);
+			className: "edit-search"
+		}, React.createElement("div", null, "Price between " + this.state.priceMin + "$ and " + this.state.priceMax + "$"), React.createElement("input", {
+			type: "range",
+			name: "points",
+			min: "0",
+			max: "3950",
+			step: "50",
+			value: this.state.priceMin,
+			onChange: this.changeMinPrice
+		}), React.createElement("input", {
+			type: "range",
+			name: "points",
+			min: "50",
+			max: "4000",
+			step: "50",
+			value: this.state.priceMax,
+			onChange: this.changeMaxPrice
+		}), React.createElement("hr", null), React.createElement(starsLayout, {
+			stars: this.state.stars,
+			editable: true
+		}));
 	}
 });
--- react_components/search-full-room.js
+++ react_components/search-full-room.js
@@ -7,59 +7,36 @@ var React = require("react"),
 
 module.exports = React.createClass({
 	getInitialState: function() {
-		return {
-			roomMin: 1,
-			roomMax: 5,
-			stars: 5,
-		};
+		return {roomMin: 1, roomMax: 5, stars: 5};
 	},
 	changeMinRoom: function(event) {
-		this.setState({
-			roomMin: event.target.value
-		});
+		this.setState({roomMin: event.target.value});
 	},
 	changeMaxRoom: function(event) {
-		this.setState({
-			roomMax: event.target.value
-		});
+		this.setState({roomMax: event.target.value});
 	},
 	render: function() {
 		return React.createElement("div", {
-				className: "search-new-container-room"
-			},
-			React.createElement("i", {
-				className: "fa fa-bed"
-			}),
-			React.createElement("div", {
-				className: "edit-search-bed-caption"
-			}, "Number of room between " + this.state.roomMin + " room and " + this.state.roomMax + " rooms"),
-			React.createElement("hr", {
-				className: "edit-search-room-caption-sep"
-			}),
-			React.createElement(
-				starsLayout, {
-					stars: this.state.stars,
-					editable: true
-				}
-			),
-			React.createElement("input", {
-				type: "range",
-				name: "points",
-				min: "1",
-				max: "4",
-				step: "1",
-				value: this.state.roomMin,
-				onChange: this.changeMinRoom
-			}),
-			React.createElement("input", {
-				type: "range",
-				name: "points",
-				min: "1",
-				max: "5",
-				step: "1",
-				value: this.state.roomMax,
-				onChange: this.changeMaxRoom
-			})
-		);
+			className: "edit-search"
+		}, React.createElement("div", null, "Number of room between " + this.state.roomMin + " room and " + this.state.roomMax + " rooms"), React.createElement("input", {
+			type: "range",
+			name: "points",
+			min: "1",
+			max: "4",
+			step: "1",
+			value: this.state.roomMin,
+			onChange: this.changeMinRoom
+		}), React.createElement("input", {
+			type: "range",
+			name: "points",
+			min: "1",
+			max: "5",
+			step: "1",
+			value: this.state.roomMax,
+			onChange: this.changeMaxRoom
+		}), React.createElement("hr", null), React.createElement(starsLayout, {
+			stars: this.state.stars,
+			editable: true
+		}));
 	}
 });
