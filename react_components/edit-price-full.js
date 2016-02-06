9223c36cd0224496d44b1b2c665daf1b1e249363
Julien Christin <jul.christin@gmail.com>
2016-02-06 10:47:04 -0500

    Add bootstrap-slider for price and room criteria pages

--- react_components/edit-price-full.js
+++ react_components/edit-price-full.js
@@ -3,39 +3,37 @@
 "use strict";
 
 var React = require("react"),
+	Slider = require("bootstrap-slider"),
 	starsLayout = require("./edit-stars");
 
 module.exports = React.createClass({
-	changeMinPrice: function(event) {
-		this.props.criterion.min = event.target.value;
-		this.forceUpdate();
-	},
-	changeMaxPrice: function(event) {
-		this.props.criterion.max = event.target.value;
-		this.forceUpdate();
+	componentDidMount: function() {
+		this.slider = new Slider("#slider-price", {
+			min: 0,
+			max: 4000,
+			step: 50,
+			value: [this.props.criterion.min, this.props.criterion.max],
+			tooltip: "hide"
+		});
+
+		this.slider.on("slide", function(event) {
+			this.props.criterion.min = event[0];
+			this.props.criterion.max = event[1];
+			this.forceUpdate();
+		}.bind(this));
 	},
 	render: function() {
-		return React.createElement("div", {
-			className: "edit-search"
-		}, React.createElement("div", null, "Price between " + this.props.criterion.min + "$ and " + this.props.criterion.max + "$"), React.createElement("input", {
-			type: "range",
-			name: "points",
-			min: "0",
-			max: "3950",
-			step: "50",
-			value: this.props.criterion.min,
-			onChange: this.changeMinPrice
-		}), React.createElement("input", {
-			type: "range",
-			name: "points",
-			min: "50",
-			max: "4000",
-			step: "50",
-			value: this.props.criterion.max,
-			onChange: this.changeMaxPrice
-		}), React.createElement("hr", null), React.createElement(starsLayout, {
-			stars: this.props.criterion.stars,
-			editable: true
-		}));
+		return React.createElement("div", null,
+			React.createElement("div", null, "Price between " + this.props.criterion.min + "$ and " + this.props.criterion.max + "$"),
+			React.createElement("input", {
+				type: "text",
+				id: "slider-price"
+			}),
+			React.createElement("hr", null),
+			React.createElement(starsLayout, {
+				stars: this.props.criterion.stars,
+				editable: true
+			})
+		);
 	}
 });
--- react_components/edit-room-full.js
+++ react_components/edit-room-full.js
@@ -3,39 +3,37 @@
 "use strict";
 
 var React = require("react"),
+	Slider = require("bootstrap-slider"),
 	starsLayout = require("./edit-stars");
 
 module.exports = React.createClass({
-	changeMinRoom: function(event) {
-		this.props.criterion.min = event.target.value;
-		this.forceUpdate();
-	},
-	changeMaxRoom: function(event) {
-		this.props.criterion.max = event.target.value;
-		this.forceUpdate();
+	componentDidMount: function() {
+		this.slider = new Slider("#slider-room", {
+			min: 1,
+			max: 5,
+			step: 1,
+			value: [this.props.criterion.min, this.props.criterion.max],
+			tooltip: "hide"
+		});
+
+		this.slider.on("slide", function(event) {
+			this.props.criterion.min = event[0];
+			this.props.criterion.max = event[1];
+			this.forceUpdate();
+		}.bind(this));
 	},
 	render: function() {
-		return React.createElement("div", {
-			className: "edit-search"
-		}, React.createElement("div", null, "Number of room between " + this.props.criterion.min + " room and " + this.props.criterion.max + " rooms"), React.createElement("input", {
-			type: "range",
-			name: "points",
-			min: "1",
-			max: "4",
-			step: "1",
-			value: this.props.criterion.min,
-			onChange: this.changeMinRoom
-		}), React.createElement("input", {
-			type: "range",
-			name: "points",
-			min: "1",
-			max: "5",
-			step: "1",
-			value: this.props.criterion.max,
-			onChange: this.changeMaxRoom
-		}), React.createElement("hr", null), React.createElement(starsLayout, {
-			stars: this.props.criterion.stars,
-			editable: true
-		}));
+		return React.createElement("div", null,
+			React.createElement("div", null, "Number of room between " + this.props.criterion.min + " room and " + this.props.criterion.max + " rooms"),
+			React.createElement("input", {
+				type: "text",
+				id: "slider-room",
+			}),
+			React.createElement("hr", null),
+			React.createElement(starsLayout, {
+				stars: this.props.criterion.stars,
+				editable: true
+			})
+		);
 	}
 });
