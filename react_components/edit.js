2885d449bbadb2dcd150d8b77a30aa3ed8d86b6f
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2016-02-07 13:37:36 -0500

    Added + icon to allow adding a new criteria zone (next step is to allow to remove/delete a criterion)

--- react_components/edit.js
+++ react_components/edit.js
@@ -32,31 +32,35 @@ module.exports = React.createClass({
 			})
 		);
 	},
-	generateLayout: function(criteria) {
-		var zoneIndex = 1;
-		var layout = _.map(criteria, _.bind(function(criterion, i) {
-			switch (criterion.type) {
-				case "price":
-					return this.generateEditLayout(EditPrice, i, criterion);
+	generateLayout: function() {
+		if (this.state.criteria) {
+			var zoneIndex = 1;
+			var layout = _.map(this.state.criteria, _.bind(function(criterion, i) {
+				switch (criterion.type) {
+					case "price":
+						return this.generateEditLayout(EditPrice, i, criterion);
 
-				case "room":
-					return this.generateEditLayout(EditRoom, i, criterion);
+					case "room":
+						return this.generateEditLayout(EditRoom, i, criterion);
 
-				case "zone":
-					criterion.id = i;
-					criterion.title = "Search zone " + zoneIndex;
-					++zoneIndex;
-					return this.generateEditLayout(EditZone, i, criterion);
+					case "zone":
+						criterion.id = i;
+						criterion.title = "Search zone " + zoneIndex;
+						++zoneIndex;
+						return this.generateEditLayout(EditZone, i, criterion);
 
-				default:
-					// Not handled yet
-					console.log(criterion.type);
-			}
-		}, this));
+					default:
+						// Not handled yet
+						console.log(criterion.type);
+				}
+			}, this));
 
-		this.setState({
-			content: layout
-		});
+			return layout;
+		} else if (this.state.requestGet) {
+			return "No criteria found !";
+		} else {
+			return "";
+		}
 	},
 	componentDidMount: function() {
 		request
@@ -65,26 +69,57 @@ module.exports = React.createClass({
 				if (err) {
 					console.log(err);
 				} else {
-					this.generateLayout(res.body);
+					this.setState({
+						requestGet: true,
+						criteria: res.body
+					});
 				}
 			}.bind(this));
 	},
 	getInitialState: function() {
 		return {
-			content: "No criteria found !"
+			requestGet: false,
+			criteria: undefined
 		};
+	},
+	handleAddZone: function() {
+
+		var criterion = {
+			type: "zone",
+			stars: 5,
+			borough: "",
+			polygon: []
+		};
+
+		this.state.criteria.push(criterion);
+
+		request
+			.post("/api/search/criteria")
+			.send(this.state.criteria)
+			.end(function(err) {
+				if (err) {
+					console.log(err);
+				}
+
+				this.props.history.pushState(
+					null, "/search/edit/" + (this.state.criteria.length)
+				);
+			}.bind(this));
 	},
 	render: function() {
 		return React.createElement("div", {
 				className: "edit-search-container"
-			},
+			}, React.createElement("i", {
+				className: "fa fa-plus-square",
+				onClick: this.handleAddZone
+			}),
 			React.createElement(Masonry, {
 				className: "masonry",
 				options: {
 					isFitWidth: true
 				},
 				disableImagesLoaded: false
-			}, this.state.content)
+			}, this.generateLayout())
 		);
 	}
 });
