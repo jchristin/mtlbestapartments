833bf2dcbf65d26580315e3a4e072af25f38c82b
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2016-02-21 22:36:44 -0500

    Added "dummy" button in the Search page to allow switch between card and map view.

--- react_components/search.js
+++ react_components/search.js
@@ -11,33 +11,34 @@ module.exports = React.createClass({
 	contextTypes: {
 		router: React.PropTypes.object.isRequired
 	},
-	getResult: function () {
-		request
-			.get("/api/search/result")
-			.end(function(err, res) {
-				if (err) {
-					if (err.status === 404) {
-						this.context.router.push("/search/new");
-					} else {
-						console.log(err);
-					}
+	getResult: function() {
+		request.get("/api/search/result").end(function(err, res) {
+			if (err) {
+				if (err.status === 404) {
+					this.context.router.push("/search/new");
 				} else {
-					if(this.isMounted) {
-						if(res.body === null) {
-							// Refresh every seconds to check if matching is done.
-							this.timer = setTimeout(this.getResult(), 1000);
-						} else {
-							// Refresh every 5 seconds to check for new result.
-							this.timer = setTimeout(this.getResult(), 5000);
-						}
-
-						this.setState({apartments: res.body});
-					}
+					console.log(err);
 				}
-			}.bind(this));
+			} else {
+				if (this.isMounted) {
+					if (res.body === null) {
+						// Refresh every seconds to check if matching is done.
+						this.timer = setTimeout(this.getResult(), 1000);
+					} else {
+						// Refresh every 5 seconds to check for new result.
+						this.timer = setTimeout(this.getResult(), 5000);
+					}
+
+					this.setState({apartments: res.body});
+				}
+			}
+		}.bind(this));
+	},
+	handleLayoutChange: function(type) {
+		this.setState({layoutType: type});
 	},
 	getInitialState: function() {
-		return {apartments: null};
+		return {apartments: null, layoutType: "card"};
 	},
 	componentDidMount: function() {
 		this.isMounted = true;
@@ -50,19 +51,25 @@ module.exports = React.createClass({
 	render: function() {
 		var content;
 
-		if(this.state.apartments === null ) {
+		if (this.state.apartments === null) {
 			content = React.createElement("div", null, "Finding matches...");
-		} else if(this.state.apartments.length === 0 ) {
+		} else if (this.state.apartments.length === 0) {
 			content = React.createElement("div", null, "No match found.");
 		} else {
-			content = React.createElement(Layout, {apartments: this.state.apartments});
+			content = React.createElement(Layout, {
+				apartments: this.state.apartments,
+				type: this.state.layoutType
+			});
 		}
 
-		return React.createElement("div", null,
-			React.createElement(Link, {
-				to: "/search/edit"
-			}, "Edit"),
-			content
-		);
+		return React.createElement("div", null, React.createElement(Link, {
+			to: "/search/edit"
+		}, "Edit"), React.createElement("i", {
+			className: "fa fa-square-o",
+			onClick: this.handleLayoutChange.bind(this, "card")
+		}), React.createElement("i", {
+			className: "fa fa-map",
+			onClick: this.handleLayoutChange.bind(this, "map")
+		}), content);
 	}
 });
