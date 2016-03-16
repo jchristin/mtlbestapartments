3101c748e138e17a6692e5421d669be1c455ddf8
Julien Christin <jul.christin@gmail.com>
2016-03-15 20:30:45 -0400

    Improve design of signin page

--- react_components/signin.js
+++ react_components/signin.js
@@ -6,49 +6,66 @@ var React = require("react"),
 	Link = require("react-router").Link;
 
 module.exports = React.createClass({
-	render: function render() {
-		return React.createElement(
-			"form", {
-				action: "/api/signin" + this.props.location.search,
-				method: "post"
+	render: function() {
+		return React.createElement("div", {
+				className: "row"
 			},
-			React.createElement(
-				"div",
-				null,
-				React.createElement(
-					"label",
-					null,
-					"Email:"
+			React.createElement("div", {
+					className: "col-xs-center"
+				},
+				React.createElement("h2", {
+					className: "text-xs-center"
+				}, "Sign in to Fleub"),
+				React.createElement("div", {
+						className: "card"
+					},
+					React.createElement("div", {
+							className: "card-block"
+						},
+						React.createElement("form", {
+								action: "/api/signin" + this.props.location.search,
+								method: "post"
+							},
+							React.createElement("div", {
+									className: "form-group"
+								},
+								React.createElement("label", null, "Email"),
+								React.createElement("input", {
+									className: "form-control",
+									type: "text",
+									name: "username"
+								})
+							),
+							React.createElement("div", {
+									className: "form-group"
+								},
+								React.createElement("label", null, "Password"),
+								React.createElement("input", {
+									className: "form-control",
+									type: "password",
+									name: "password"
+								})
+							),
+							React.createElement("button", {
+								className: "btn btn-lg btn-primary btn-block",
+								type: "submit"
+							}, "Sign in")
+						)
+					)
 				),
-				React.createElement("input", {
-					type: "text",
-					name: "username"
-				})
-			),
-			React.createElement(
-				"div",
-				null,
-				React.createElement(
-					"label",
-					null,
-					"Password:"
-				),
-				React.createElement("input", {
-					type: "password",
-					name: "password"
-				})
-			),
-			React.createElement(
-				"div",
-				null,
-				React.createElement("input", {
-					type: "submit",
-					defaultValue: "Sign In"
-				})
-			),
-			React.createElement(Link, {
-				to: "/signup"
-			}, "Sign Up")
+				React.createElement("div", {
+						className: "card"
+					},
+					React.createElement("div", {
+							className: "card-block"
+						},
+						"New to Fleub? ",
+						React.createElement(Link, {
+							to: "/signup"
+						}, "Create an account"), "."
+					)
+				)
+			)
 		);
 	}
 });
