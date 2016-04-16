5a05239214646456972a105086fd8d329b48c905
Julien Christin <jul.christin@gmail.com>
2016-04-16 08:21:09 -0400

    Fix promise in database

--- database.js
+++ database.js
@@ -5,12 +5,10 @@ var mongoClient = require("mongodb").MongoClient;
 module.exports.connect = function() {
 	var promise = mongoClient.connect(process.env.MONGODB_URL);
 
-	promise.then(function(database) {
+	return promise.then(function(database) {
 		module.exports.instance = database;
 		module.exports.users = database.collection("users");
 		module.exports.searches = database.collection("searches");
 		module.exports.apartments = database.collection("apartments");
 	});
-
-	return promise;
 };
