2b20297e9b102fe7acdbe1ac1bb9e00dc750b91a
Mathieu Albrespy <mathieu.albrespy@gmail.com>
2016-05-14 18:16:21 -0400

    Fixed marker redraw after login
    Marker are drawn when componentDidMount is called. After login
    componentDidMount  is not called. I moved markers draw into a function
    to be called when componentDidMount and componentDidUpdate is executed.

--- react_components/layout-map.js
+++ react_components/layout-map.js
@@ -10,34 +10,7 @@ module.exports = React.createClass({
 	contextTypes: {
 		router: React.PropTypes.object.isRequired
 	},
-	componentDidMount: function() {
-
-		// Create marker icons (dot and pin)
-		this.markerIconDot = {
-			url: "img/marker-dot.png",
-			size: new google.maps.Size(10, 10),
-			anchor: new google.maps.Point(5, 5)
-		};
-
-		this.markerIconDotViewed = {
-			url: "img/marker-dot-viewed.png",
-			size: new google.maps.Size(10, 10),
-			anchor: new google.maps.Point(5, 5)
-		};
-
-		this.markerIcon = this.markerIconDot;
-
-		// Create the map.
-		this.map = new google.maps.Map(document.getElementById("map-canvas-full"), mapSettings.options);
-
-		// Apply style to the map.
-		var styledMap = new google.maps.StyledMapType(require("./map-style"));
-		this.map.mapTypes.set("map-style", styledMap);
-		this.map.setMapTypeId("map-style");
-
-		this.bounds = new google.maps.LatLngBounds();
-
-		// Place marker.
+	placeMarker: function() {
 		this.marker = [];
 		this.marker = _.map(this.props.apartments, _.bind(function(apart) {
 			var position = new google.maps.LatLng(apart.coord[1], apart.coord[0]);
@@ -54,6 +27,40 @@ module.exports = React.createClass({
 		}, this));
 
 		this.map.fitBounds(this.bounds);
+	},
+	componentDidUpdate: function() {
+		this.placeMarker();
+	},
+	componentDidMount: function() {
+		// Create marker icons (dot and pin)
+		this.markerIconDot = {
+			url: "img/marker-dot.png",
+			size: new google.maps.Size(10, 10),
+			anchor: new google.maps.Point(5, 5)
+		};
+
+		this.markerIconDotViewed = {
+			url: "img/marker-dot-viewed.png",
+			size: new google.maps.Size(10, 10),
+			anchor: new google.maps.Point(5, 5)
+		};
+
+		this.markerIcon = this.markerIconDot;
+
+		// Create the map.
+		this.map = new google.maps.Map(
+			document.getElementById("map-canvas-full"),
+			mapSettings.options);
+
+		// Apply style to the map.
+		var styledMap = new google.maps.StyledMapType(require("./map-style"));
+		this.map.mapTypes.set("map-style", styledMap);
+		this.map.setMapTypeId("map-style");
+
+		this.bounds = new google.maps.LatLngBounds();
+
+		// Place marker.
+		this.placeMarker();
 	},
 	render: function() {
 		return React.DOM.div({id: "map-canvas-full"});
