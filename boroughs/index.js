63573e9d9d3bfc8566d46491808f1fc19542dd0c
Julien Christin <jul.christin@gmail.com>
2016-05-21 11:42:46 -0400

    Consolidate borough's names

--- boroughs/index.js
+++ boroughs/index.js
@@ -13,7 +13,7 @@ module.exports = {
 	},
 	"baie-durfe": {
 		coord: require("./baie-durfe"),
-		name: "Baie-durfe",
+		name: "Baie-d'Urfé",
 		type: "Polygon",
 	},
 	"beaconsfield": {
@@ -23,17 +23,17 @@ module.exports = {
 	},
 	"cote-des-neiges-notre-dame-de-grace": {
 		coord: require("./cote-des-neiges-notre-dame-de-grace"),
-		name: "Cote-des-neiges-notre-dame-de-grace",
+		name: "Côte-des-Neiges–Notre-Dame-de-Grâce",
 		type: "Polygon",
 	},
 	"cote-saint-luc": {
 		coord: require("./cote-saint-luc"),
-		name: "Cote-saint-luc",
+		name: "Côte Saint-Luc",
 		type: "Polygon",
 	},
 	"dollard-des-ormeaux": {
 		coord: require("./dollard-des-ormeaux"),
-		name: "Dollard-des-ormeaux",
+		name: "Dollard-des-Ormeaux",
 		type: "Polygon",
 	},
 	"dorval": {
@@ -48,7 +48,7 @@ module.exports = {
 	},
 	"ile-bizard-sainte-genevieve": {
 		coord: require("./ile-bizard-sainte-genevieve"),
-		name: "Ile-bizard-sainte-genevieve",
+		name: "L'Île-Bizard–Sainte-Geneviève",
 		type: "Polygon",
 	},
 	"kirkland": {
@@ -63,42 +63,42 @@ module.exports = {
 	},
 	"lasalle": {
 		coord: require("./lasalle"),
-		name: "Lasalle",
+		name: "LaSalle",
 		type: "Polygon",
 	},
 	"le-plateau-mont-royal": {
 		coord: require("./le-plateau-mont-royal"),
-		name: "Le-plateau-mont-royal",
+		name: "Le Plateau-Mont-Royal",
 		type: "Polygon",
 	},
 	"le-sud-ouest": {
 		coord: require("./le-sud-ouest"),
-		name: "Le-sud-ouest",
+		name: "Le Sud-Ouest",
 		type: "Polygon",
 	},
 	"mercier-hochelaga-maisonneuve": {
 		coord: require("./mercier-hochelaga-maisonneuve"),
-		name: "Mercier-hochelaga-maisonneuve",
+		name: "Mercier–Hochelaga-Maisonneuve",
 		type: "Polygon",
 	},
 	"montreal-est": {
 		coord: require("./montreal-est"),
-		name: "Montreal-est",
+		name: "Montréal-Est",
 		type: "Polygon",
 	},
 	"montreal-nord": {
 		coord: require("./montreal-nord"),
-		name: "Montreal-nord",
+		name: "Montréal-Nord",
 		type: "Polygon",
 	},
 	"montreal-ouest": {
 		coord: require("./montreal-ouest"),
-		name: "Montreal-ouest",
+		name: "Montreal-Ouest",
 		type: "Polygon",
 	},
 	"mont-royal": {
 		coord: require("./mont-royal"),
-		name: "Mont-royal",
+		name: "Mont-Royal",
 		type: "Polygon",
 	},
 	"outremont": {
@@ -108,37 +108,37 @@ module.exports = {
 	},
 	"pierrefonds-roxboro": {
 		coord: require("./pierrefonds-roxboro"),
-		name: "Pierrefonds-roxboro",
+		name: "Pierrefonds-Roxboro",
 		type: "Polygon",
 	},
 	"pointe-claire": {
 		coord: require("./pointe-claire"),
-		name: "Pointe-claire",
+		name: "Pointe-Claire",
 		type: "Polygon",
 	},
 	"riviere-des-prairies-pointe-aux-trembles": {
 		coord: require("./riviere-des-prairies-pointe-aux-trembles"),
-		name: "Riviere-des-prairies-pointe-aux-trembles",
+		name: "Rivière-des-Prairies–Pointe-aux-Trembles",
 		type: "Polygon",
 	},
 	"rosemont-la-petite-patrie": {
 		coord: require("./rosemont-la-petite-patrie"),
-		name: "Fosemont-la-petite-patrie",
+		name: "Rosemont–La Petite-Patrie",
 		type: "Polygon",
 	},
 	"sainte-anne-de-bellevue": {
 		coord: require("./sainte-anne-de-bellevue"),
-		name: "Sainte-anne-de-bellevue",
+		name: "Sainte-Anne-de-Bellevue",
 		type: "Polygon",
 	},
 	"saint-laurent": {
 		coord: require("./saint-laurent"),
-		name: "Saint-laurent",
+		name: "Saint-Laurent",
 		type: "Polygon",
 	},
 	"saint-leonard": {
 		coord: require("./saint-leonard"),
-		name: "Saint-leonard",
+		name: "Saint-Léonard",
 		type: "Polygon",
 	},
 	"senneville": {
@@ -153,12 +153,12 @@ module.exports = {
 	},
 	"ville-marie": {
 		coord: require("./ville-marie"),
-		name: "Ville-marie",
+		name: "Ville-Marie",
 		type: "Polygon",
 	},
 	"villeray-saint-michel-parc-extension": {
 		coord: require("./villeray-saint-michel-parc-extension"),
-		name: "Villeray-saint-michel-parc-extension",
+		name: "Villeray–Saint-Michel–Parc-Extension",
 		type: "Polygon",
 	},
 	"westmount": {
