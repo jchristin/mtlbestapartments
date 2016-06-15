19aa3a3e1368ff9af619cb21b7cf70f276d758d1
Julien Christin <jul.christin@gmail.com>
2016-06-15 13:53:56 -0400

    Fix layout map
    Dots were not displayed and old dots were not removed

--- react_components/layout-map.js
+++ react_components/layout-map.js
@@ -12,15 +12,18 @@ module.exports = React.createClass({
 		lang: React.PropTypes.string
 	},
 	placeMarker: function() {
-		this.marker = [];
+		_.forEach(this.markers, function(marker) {
+			marker.setMap(null);
+		});
+
 		this.marker = _.map(this.props.apartments, _.bind(function(apart) {
 			var position = new google.maps.LatLng(apart.coord[1], apart.coord[0]);
 
 			this.bounds.extend(position);
 
-			var marker = new google.maps.Marker({position: position, map: this.map, icon: this.markerIcon});
+			var marker = new google.maps.Marker({position: position, map: this.map, icon: this.markerIconDot});
 
-			marker.addListener('click', function() {
+			marker.addListener("click", function() {
 				this.context.router.push("/" + this.context.lang + "/a/" + apart._id);
 			}.bind(this));
 
@@ -33,20 +36,12 @@ module.exports = React.createClass({
 		this.placeMarker();
 	},
 	componentDidMount: function() {
-		// Create marker icons (dot and pin)
+		// Create marker icons (dot and pin).
 		this.markerIconDot = {
-			url: "img/marker-dot.png",
+			url: "/img/marker-dot.png",
 			size: new google.maps.Size(10, 10),
 			anchor: new google.maps.Point(5, 5)
 		};
-
-		this.markerIconDotViewed = {
-			url: "img/marker-dot-viewed.png",
-			size: new google.maps.Size(10, 10),
-			anchor: new google.maps.Point(5, 5)
-		};
-
-		this.markerIcon = this.markerIconDot;
 
 		// Create the map.
 		this.map = new google.maps.Map(
